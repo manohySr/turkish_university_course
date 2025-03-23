8c83c2fb45df83e34bac34de5c45ebb5fb3a83d6
manohySr <manohyrajaonah@gmail.com>
2025-03-23 20:42:49 +0300

    refactor: remove unecessery code

--- front/components/outcome-course.tsx
+++ front/components/outcome-course.tsx
@@ -1,11 +1,3 @@
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 import { Briefcase, ChevronRight, LightbulbIcon, Users } from "lucide-react";
 
 const OutCome = () => {
